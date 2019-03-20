8af7498fd7191186550a772eded66a7f0388ccc3
haiktpm <haitv2809@gmail.com>
2019-03-21 01:16:19 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test Hello</title>
  </head>
  <body>
- <?php $envi = getenv('Environment'); echo '<p>Hello World environment: </p>'.$envi; ?> 
+ <?php $envi = var_dump($_ENV['Environment']); echo '<p>Hello World environment: </p>'.$envi; ?> 
  </body>
 </html>
